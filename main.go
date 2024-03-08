1a5c71d62c815f209430248328efec4e3d91df16
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-09 03:15:43 +0700

    :sparkles: add world render

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/Rayato159/nevers-space-shooting/src/systems"
+	"github.com/Rayato159/nevers-space-shooting/src/world"
+)
+
 func main() {
+	world.Setup()
+
+	for {
+		world.Draw()
+
+		systems.Update()
+	}
 }
