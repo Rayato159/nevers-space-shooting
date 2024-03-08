61ff8b69014df237ad474bbd2c93107d2e1fe870
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-09 02:44:19 +0700

    :sparkles: test world drawing and update

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/Rayato159/nevers-space-shooting/src/systems"
+	"github.com/Rayato159/nevers-space-shooting/src/world"
+)
+
 func main() {
+	for {
+		world.Draw()
+		systems.Update()
+	}
 }
