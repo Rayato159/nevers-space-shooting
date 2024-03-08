e3f7c77ee6834ef6c191b48edb15d2ee2f46993c
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-09 03:25:49 +0700

    :package: change world2D to public var

--- src/world/world.go
+++ src/world/world.go
@@ -7,7 +7,7 @@ const (
 	HEIGHT = 10
 )
 
-var world2D = [WIDTH + 2][HEIGHT + 2]rune{}
+var World2D = [WIDTH + 2][HEIGHT + 2]rune{}
 
 func Setup() {
 	for y := range HEIGHT + 2 {
@@ -22,7 +22,7 @@ func Setup() {
 func Draw() {
 	for y := range HEIGHT + 2 {
 		for x := range WIDTH + 2 {
-			fmt.Print(string(world2D[y][x]))
+			fmt.Print(string(World2D[y][x]))
 		}
 
 		fmt.Println()
@@ -32,7 +32,7 @@ func Draw() {
 func setupHorizontalLine(y int) {
 	if y == 0 || y == HEIGHT+2-1 {
 		for x := range WIDTH + 2 {
-			world2D[y][x] = 'x'
+			World2D[y][x] = 'x'
 		}
 	}
 }
@@ -41,8 +41,8 @@ func setupVerticalLine(y, x int) {
 	if y == 0 || y == HEIGHT+2-1 {
 		return
 	} else if x == 0 || x == WIDTH+2-1 {
-		world2D[y][x] = 'x'
+		World2D[y][x] = 'x'
 	} else {
-		world2D[y][x] = ' '
+		World2D[y][x] = ' '
 	}
 }
